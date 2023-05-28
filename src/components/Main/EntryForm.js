286280f167b92be28d7257852a7c508ed4947ed2
Christoph Leuker <cleuker87@gmail.com>
2023-05-28 15:39:53 +0200

    add button and card

--- src/components/Main/EntryForm.js
+++ src/components/Main/EntryForm.js
@@ -1,11 +1,16 @@
+import "./EntryForm.css";
 export default function EntryForm({ inputname, inputname2, motto, notes }) {
   return (
     <>
-      <form>
+      <h2 className="header__form">NEW ENTRY</h2>
+      <form className="entry-form__container">
         <label htmlFor={inputname}>{motto}</label>
         <input type="text" name={inputname}></input>
         <label htmlFor={inputname2}>{notes}</label>
         <textarea rows="4" name={inputname2}></textarea>
+        <button type="submit" className="create__button">
+          Create
+        </button>
       </form>
     </>
   );
---
+++ src/components/Main/EntrySection.js
@@ -0,0 +1,14 @@
+import TabBar from "./TabBar";
+import EntryList from "./EntryList";
+
+export default function EntrySection() {
+  return (
+    <>
+      <TabBar />
+      <EntryList
+        headtitle="Hier könnte ihre Überschrift stehen"
+        description="und hier steht dann ihr ganzer Text und so anderer kram"
+      />
+    </>
+  );
+}
