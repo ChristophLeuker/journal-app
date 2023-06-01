676df21eb4fcb00b54ff1b84a55b85bebc415a37
Christoph Leuker <cleuker87@gmail.com>
2023-06-01 11:00:53 +0200

    add new entries funktion

--- src/components/Main/EntryForm/EntryForm.js
+++ src/components/Main/EntryForm/EntryForm.js
@@ -1,9 +1,23 @@
 import "./EntryForm.css";
-export default function EntryForm({ inputname, inputname2, motto, notes }) {
+export default function EntryForm({
+  inputname,
+  inputname2,
+  motto,
+  notes,
+  onAddEntries,
+}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddEntries(data);
+    event.target.reset();
+  }
+
   return (
     <>
       <h2 className="header__form">NEW ENTRY</h2>
-      <form className="entry-form__container">
+      <form className="entry-form__container" onSubmit={handleSubmit}>
         <label htmlFor={inputname}>{motto}</label>
         <input type="text" name={inputname}></input>
         <label htmlFor={inputname2}>{notes}</label>
--- src/components/Main/EntrySection/TabBar/TabBar.js
+++ src/components/Main/EntrySection/TabBar/TabBar.js
@@ -1,14 +1,14 @@
 import "./TabBar.css";
-import Badge from "./Badge.js";
+import Badge from "./Badge/Badge";
 
 export default function TabBar() {
   return (
     <>
       <section className="tab-bar__container">
         <p>All Entries </p>
-        <Badge count="5" />
+        <Badge count="5" isActive={true} />
         <p>Favorites</p>
-        <Badge count="0" />
+        <Badge count="0" isActice={false} />
       </section>
     </>
   );
